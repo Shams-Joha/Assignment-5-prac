2391bb024f6adb08c2c4620c1615095297e7e4d8
Shams-Joha <shamsjoha3@gmail.com>
2024-09-22 00:11:52 +0600

    increase & decrease seat count

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const selectedSeat = document.getElementById('selected-seat');
+const selectedSeatEl = document.getElementById('selected-seat');
 
-function handleSelectSeat(event) {
-   selectedSeat.innerHTML = `
-   <li class= " text-green-500 font-normal flex justify-between">
-   <span>${event.innerText}</span>
-   <span>Economy</span>
-   <span>550</span>
-   </li>
+let bookCounter = document.getElementById('total-booked');
+
+
+// function handleSelectSeat(event) {
+//    selectedSeat.innerHTML = `
+//    <li class= " text-green-500 font-normal flex justify-between">
+//    <span>${event.innerText}</span>
+//    <span>Economy</span>
+//    <span>550</span>
+//    </li>
    
-   `
+//    `
+// }
+let selectedSeat = [];
+function handleSelectSeat(event){
+   event.classList.add('bg-green-400');
+   event.classList.add('text-white');
+
+   selectedSeat.push(event.innerText);
+
+   bookCounter.innerText = selectedSeat.length;
+
+   // Available seats Code:
+   // const availSeatValue = parseFloat(availableSeatContainer.innerText);
+   // const newAvailable = availSeatValue -1;
+   // availableSeatContainer.innerText = newAvailable;
+
+   
 }
+
+
 
